96f18d0e2963202b9b69a84ab58447c7190f3815
rdevaramani3 <rdevaramani3@gatech.edu>
2024-07-25 00:17:14 -0400

    jsonupdate

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/rajeshwari179.github.io/' : '',
   trailingSlash: true,
 };
